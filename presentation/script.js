10cb32297faaeee3c9305bb884e18eb75e533e6e
MilenaGiachetti <giachettimilena@gmail.com>
2022-03-12 22:36:26 -0300

    Add presentation lang change base

---
+++ presentation/script.js
@@ -0,0 +1,23 @@
+const changeDisplay = (lang, display) => {
+    const texts = document.querySelectorAll(`[lang='${lang}']:not(html)`);
+    texts.forEach((text) => {
+        text.style.display = display;
+    })
+}
+const toggleTexts = (lang) => {
+    if (lang === 'en') {
+        changeDisplay('en', '');
+        changeDisplay('es', 'none');
+    } else {
+        changeDisplay('es', '');
+        changeDisplay('en', 'none');
+    }
+}
+
+document.getElementById("es-btn").addEventListener("click", () => {
+    toggleTexts("es");
+})
+document.getElementById("en-btn").addEventListener("click", () => {
+    toggleTexts("en");
+})
+toggleTexts("es");
