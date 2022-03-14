089257a974e068326230b069a0bd8124628372df
MilenaGiachetti <giachettimilena@gmail.com>
2022-03-14 00:29:54 -0300

    Add animations and hover styles

--- presentation/script.js
+++ presentation/script.js
@@ -34,3 +34,54 @@ navToggle.addEventListener("click", () => {
     }
     navToggle.classList.toggle("nav_toggle-close");
 })
+
+// ANIMATIONS with GSAP
+gsap.registerPlugin(ScrollTrigger);
+// Greetings section appear animation
+const greetingsTL = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#greetings"
+    }
+});
+
+// Content
+greetingsTL.from(".greeting-container", {
+    opacity: 0,
+    x: -300, 
+}).to(".greeting-container", {
+    duration: 0.3, 
+    opacity: 1,
+    x: 0, 
+    ease: "none", 
+});
+
+// Image
+greetingsTL.from(".image-container", {
+    opacity: 0,
+    x: 300, 
+}, 0).to(".image-container", {
+    duration: 0.2, 
+    opacity: 1,
+    x: 0, 
+    ease: "none", 
+});
+
+// Links
+greetingsTL.from(".links-container a", {
+    opacity: 0,
+    scale: 0
+}, .2).to(".links-container a", {
+    duration: 0.2, 
+    opacity: 1,
+    scale: 1
+});
+
+// Section headings load animation
+gsap.utils.toArray("h3").forEach(el => {
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: el,
+            onEnter: () => el.classList.add("load")
+        }
+    });
+});
