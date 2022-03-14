ca92142250e5ba3e257a631a7100d424fcdc557e
MilenaGiachetti <giachettimilena@gmail.com>
2022-03-13 22:14:57 -0300

    Add mobile styles

--- presentation/script.js
+++ presentation/script.js
@@ -1,3 +1,4 @@
+// LANGUAGE
 const changeDisplay = (lang, display) => {
     const texts = document.querySelectorAll(`[lang='${lang}']:not(html)`);
     texts.forEach((text) => {
@@ -22,3 +23,14 @@ document.getElementById("en-btn").addEventListener("click", () => {
     toggleTexts("en");
 })
 toggleTexts("es");
+
+// NAV
+const navToggle = document.getElementById("nav_toggle");
+navToggle.addEventListener("click", () => {
+    if(navToggle.classList.contains("nav_toggle-close")) {
+        navToggle.setAttribute("aria-label", "Open nav");
+    } else {
+        navToggle.setAttribute("aria-label", "Close nav");
+    }
+    navToggle.classList.toggle("nav_toggle-close");
+})
